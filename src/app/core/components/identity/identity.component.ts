6aed65e71abb8338ec31ae765c49557b8e9611f1
imak101 <mftrenkler@gmail.com>
2025-01-10 19:58:12 -0700

    complete c build functionality and remove username input

--- src/app/core/components/identity/identity.component.ts
+++ src/app/core/components/identity/identity.component.ts
@@ -21,53 +21,12 @@ import {NgClass, NgIf} from "@angular/common";
 export class IdentityComponent implements AfterViewInit {
   @ViewChild('usernameDialog', {static: false}) usernameDialog: ElementRef<HTMLDialogElement> = {} as ElementRef;
 
-  usernameForm = new FormGroup(
-    {
-      username: new FormControl("",
-        [
-        Validators.required,
-        Validators.minLength(4),
-      ])
-    },
-  )
-
-  get usernameInput() {
-    return this.usernameForm.controls.username;
+  get version(): string {
+    return "v2.2.0c";
   }
 
-  hideCloseButton: boolean = false;
-  constructor(private store: Store<AppState>, private xferLocalStorage: XferLocalStorageService) {}
+  constructor() {}
 
   ngAfterViewInit(): void {
-    const username = this.xferLocalStorage.getUsername();
-    if (username === null) {
-      this.hideCloseButton = true;
-      this.usernameDialog.nativeElement.showModal();
-      return;
-    }
-    this.usernameForm.controls.username.setValue(username);
-  }
-
-  signInButtonPressed() {
-    this.hideCloseButton = false;
-    this.xferLocalStorage.setUsername(this.usernameForm.controls.username.value ?? "sampleUser");
-    this.dispatchActions();
-  }
-
-  userCanceledForm() {
-    if (this.xferLocalStorage.getUsername() !== null) return;
-
-    this.xferLocalStorage.setUsername("sampleUser");
-    this.usernameForm.controls.username.setValue("sampleUser");
-    this.dispatchActions();
-  }
-
-  private dispatchActions() {
-    this.store.dispatch(getTodayFromDatabase());
-    this.store.dispatch(getHistory());
-  }
-
-  getTooltip(): string {
-    return this.xferLocalStorage.getUsername() ?? "";
   }
 }
--- src/app/shared/services/database/xfer-database.service.ts
+++ src/app/shared/services/database/xfer-database.service.ts
@@ -2,24 +2,24 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {XferDataEntry} from "../../interfaces/xfer-data-entry";
 import {map, Observable} from "rxjs";
-import {XferLocalStorageService} from "../local-storage/xfer-local-storage.service";
 
 @Injectable({
   providedIn: 'root'
 })
-export class XferDatabaseService {/
-  constructor(private http: HttpClient, private xferLocalStorage: XferLocalStorageService) { }
+export class XferDatabaseService {
+  constructor(private http: HttpClient) { }
+  private path = "data";
 
   private defaultEntry(): XferDataEntry {
     return {
       date: new Date().toLocaleDateString(),
       callsXfer: 0,
       callsTaken: 0,
-    }
+    };
   }
 
   private convertLocalEntries(): object[] {
-    const json = JSON.parse(localStorage.getItem("dataTest") ?? JSON.stringify(this.defaultEntry()))
+    const json = JSON.parse(localStorage.getItem(this.path) ?? JSON.stringify(this.defaultEntry()));
     const keys = Object.keys(json);
 
     const output = [];
@@ -38,20 +38,20 @@ export class XferDatabaseService {/
       responseType: "text",
     }).pipe(
       map(() => {
-        return this.convertLocalEntries() as XferDataEntry[]
+        return this.convertLocalEntries() as XferDataEntry[];
       })
     );
   }
 
   saveEntry(entry: XferDataEntry) {
-    const pastData = localStorage.getItem("dataTest") ?? "{}"
+    const pastData = localStorage.getItem(this.path) ?? "{}";
     const json = JSON.parse(pastData);
     json[entry.date.replaceAll('/', '-')] = {
       callsTaken: entry.callsTaken,
       callsXfer: entry.callsXfer
-    }
-    localStorage.setItem("dataTest", JSON.stringify(json));
+    };
+    localStorage.setItem(this.path, JSON.stringify(json));
 
-    return new Observable()
+    return new Observable();
   }
 }
